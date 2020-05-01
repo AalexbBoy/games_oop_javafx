fa84d0a05b8d25cbf9f1edd8d760f228bfc4d528
alex.boy <bli@tut.by>
2020-05-01 07:40:08 +0300

    class BlackBishopTest

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -20,42 +20,5 @@ public class LogicTest {
         boolean rsl = logic.move(Cell.C1, Cell.H6);
         assertThat(rsl, is(true));
     }
-    // Проверка метода position. Создайте объект и вызовите у него м,етод position. Проверьте, что он занимает ту же ячейку,
-    // что и при создании объекта.
 
-    
-    @Test
-    public void position() {
-        Figure bishopBlack = new BishopBlack(Cell.C1);
-        Cell rsl = bishopBlack.position();
-        assertThat(rsl, is(Cell.C1));
-    }
-
-    @Test
-    //Проверка метода copy. Создайте объект и вызовите у него метод copy. Проверь,те, что, возвращенный объект имеет
-    // правильную позицию.
-    public void copy() {
-        Figure bishopBlack = new BishopBlack(Cell.C1);
-        Figure rsl = bishopBlack.copy(Cell.H6);
-        assertThat(rsl.position(), is(Cell.H6));
-    }
-
-   
-    //Для этого создайте объект с начальным положеним C1. Вызовите метод way с указанием C1 и G5. Метод должен вернуть
-    // массив из 4 клеток. D2, E3, F4, G5.
-
-    @Test
-    public void way() {
-        Figure bishopBlack = new BishopBlack(Cell.C1);
-        Cell[] rsl = bishopBlack.way(Cell.C1,Cell.G5);
-        Cell[] expected= {Cell.D2,Cell.E3,Cell.F4,Cell.G5};
-        assertThat(rsl, is(expected));
-    }
-    @Test
-    public void wayLeft() {
-        Figure bishopBlack = new BishopBlack(Cell.E3);
-        Cell[] rsl = bishopBlack.way(Cell.E3,Cell.C5);
-        Cell[] expected= {Cell.D4,Cell.C5};
-        assertThat(rsl, is(expected));
-    }
 }
